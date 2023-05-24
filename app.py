db396637882a1b5e9a955435c750d978cdf88548
Josephwhx <JosephWonghx@gmail.com>
2023-05-24 10:05:21 +0800

    include delete and put endpoints

--- app.py
+++ app.py
@@ -12,23 +12,10 @@ def get_all_stores():
     return {"stores": list(stores.values())}
 
 
-@app.get("/item") 
-def get_all_items(): 
-    return {"items": list(items.values())}
-
-
 @app.get("store/<string:store_id>")
 def get_store(store_id):
     try:
         return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
-@app.get("item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
     except KeyError:
         abort(404, message="Store not found.")
 
@@ -53,6 +40,28 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores["store_id"]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+@app.get("/item") 
+def get_all_items(): 
+    return {"items": list(items.values())}
+
+
+@app.get("item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Store not found.")
 
 
 @app.post("/item")
@@ -86,3 +95,25 @@ def create_item(name):
     items[item_id] = item
 
     return item, 201
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items["item_id"]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(404, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+    
+    try:
+        item = items[item_id]
+        item |= item_data
+    except KeyError:
+        abort(404, message="Item not found.")
