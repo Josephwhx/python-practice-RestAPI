4cd8ff07b9fedf5155a13371824570c5cfbb33a5
Josephwhx <JosephWonghx@gmail.com>
2023-05-24 11:32:15 +0800

    Decorating responses with flask_smorest & marshmellow

--- resources/item.py
+++ resources/item.py
@@ -11,11 +11,13 @@ blp = Blueprint("items", __name__, description="Operations on items")
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
 
     @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, item_data):
         for item in items.values():
             if (
@@ -33,6 +35,7 @@ class ItemList(MethodView):
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -49,6 +52,7 @@ class Item(MethodView):
 
     
     @blp.arguments(ItemUpdatedSchema)
+    @blp.response(200, ItemSchema)
     def update(self, item_data, item_id):
         try:
             item = items[item_id]
--- resources/store.py
+++ resources/store.py
@@ -10,11 +10,13 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
 
 
     @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
@@ -26,11 +28,12 @@ class StoreList(MethodView):
         store_id = uuid.uuid4().hex
         store = {**store_data, "id": store_id}
         stores[store_id] = store
-        return store, 201
+        return store
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
