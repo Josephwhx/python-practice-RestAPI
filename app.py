469b8d10fd97cbaab9542746737d56b6c6b0cdbc
Josephwhx <JosephWonghx@gmail.com>
2023-05-25 09:43:35 +0800

    include logout endpoint

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
+
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -30,6 +32,21 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "259870805717855859114298379858214927458"
     jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+    
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return(
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401
+        )
+    
 
     with app.app_context():
         db.create_all()
