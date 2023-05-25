7b7bbb740f14d3d31487975d794b7111a3365cc0
Josephwhx <JosephWonghx@gmail.com>
2023-05-25 10:13:28 +0800

    Initialize database with flask-migrate

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -27,6 +28,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     api = Api(app)
 
@@ -48,8 +50,9 @@ def create_app(db_url=None):
         )
     
 
-    with app.app_context():
-        db.create_all()
+    '''Replaced by Flask-Migrate'''
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
